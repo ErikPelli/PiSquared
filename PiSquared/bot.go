43a8ec304325a4f9e1f6ab5bf40f640fc77f4bd5
ErikPelli <erikpellizzon1@gmail.com>
2022-02-07 21:28:20 +0100

    Add menu to start

--- PiSquared/bot.go
+++ PiSquared/bot.go
@@ -27,12 +27,41 @@ func NewBot(token string) (Bot, error) {
 func (bot *Bot) InitHandlers() {
 	bot.mem = cmap.New()
 
+	var (
+		startMenu = &tb.ReplyMarkup{ResizeReplyKeyboard: true}
+		btnCS     = startMenu.Text("🖥️ Computer Science")
+		btnGeo    = startMenu.Text("🗺️️ Geography")
+		btnHis    = startMenu.Text("📜️ History")
+	)
+
+	bot.Handle(&btnCS, func(m *tb.Message) {
+		bot.handleSubject(m.Sender, computerScience)
+	})
+
+	bot.Handle(&btnGeo, func(m *tb.Message) {
+		bot.handleSubject(m.Sender, geography)
+	})
+
+	bot.Handle(&btnHis, func(m *tb.Message) {
+		bot.handleSubject(m.Sender, history)
+	})
+
 	bot.Handle("/start", func(m *tb.Message) {
-		bot.Send(m.Sender, "Welcome "+m.Chat.FirstName+"!")
+		bot.Send(m.Sender, "Welcome "+m.Chat.FirstName+"!\n\n📚 Select the school subject from the menu.", startMenu)
 		chatId := strconv.FormatInt(m.Sender.ID, 10)
 		bot.mem.Set(chatId, user{s: startMessage})
 	})
 
 	bot.Handle(tb.OnText, func(m *tb.Message) {
 	})
+}
+
+func (bot *Bot) handleSubject(u *tb.User, s subject) {
+	bot.Send(u, "✅ The \""+s.String()+"\" school subject has been set.")
+	chatId := strconv.FormatInt(u.ID, 10)
+	bot.mem.Set(chatId, user{
+		s:   subjectSelected,
+		sub: s,
+	})
+	bot.Send(u, "Get a new question using /next.")
 }
