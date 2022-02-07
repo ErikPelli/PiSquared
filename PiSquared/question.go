f7c7960acadd5d38d09d3a77b9b7301fe1368890
ErikPelli <erikpellizzon1@gmail.com>
2022-02-07 21:53:22 +0100

    Added question file

---
+++ PiSquared/question.go
@@ -0,0 +1,15 @@
+package PiSquared
+
+// getQuestions returns a new question.
+// First returned variable is the question and the second is the answer.
+func getQuestion(s subject) (string, string) {
+	// TODO
+	return "", ""
+}
+
+// Attribute a percentage score to the answer.
+// From 0 to 100%.
+func evalAnswer(question, answer string) float32 {
+	// TODO
+	return 0
+}
