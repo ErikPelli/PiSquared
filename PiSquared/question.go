cbad394f1d716ec17c39ae6971c553ba1d5deeea
ErikPelli <erikpellizzon1@gmail.com>
2022-02-08 20:58:47 +0100

    questions file parsing

--- PiSquared/question.go
+++ PiSquared/question.go
@@ -1,15 +1,48 @@
 package PiSquared
 
+import (
+	"encoding/json"
+	"math/rand"
+)
+
+type Question struct {
+	Question string `json:"question"`
+	Answer   string `json:"answer"`
+}
+
+type Questions struct {
+	ComputerScience []Question `json:"computerScience"`
+	Geography       []Question `json:"geography"`
+	History         []Question `json:"history"`
+}
+
+var questions Questions
+
+func LoadQuestions(jsonData []byte) error {
+	return json.Unmarshal(jsonData, &questions)
+}
+
 // getQuestions returns a new question.
 // First returned variable is the question and the second is the answer.
-func getQuestion(s subject) (string, string) {
+func getQuestion(s subject) Question {
 	// TODO
-	return "", ""
+	var q []Question
+	switch s {
+	case computerScience:
+		q = questions.ComputerScience
+	case geography:
+		q = questions.Geography
+	case history:
+		q = questions.History
+	default:
+		return Question{}
+	}
+	return q[rand.Intn(len(q))]
 }
 
 // Attribute a percentage score to the answer.
 // From 0 to 100%.
 func evalAnswer(question, answer string) float32 {
 	// TODO
-	return 0
+	return 50
 }
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/ErikPelli/PiSquared/PiSquared"
 	"github.com/joho/godotenv"
+	"io/ioutil"
 	"log"
 	"os"
 )
@@ -10,6 +11,15 @@ import (
 func main() {
 	if err := godotenv.Load(); err != nil {
 		log.Fatal("Error loading .env file")
+	}
+
+	questionsFile, err := os.Open(os.Getenv("QUESTIONS_FILE"))
+	if err != nil {
+		log.Fatal("Error loading questions file")
+	}
+	questionsValue, _ := ioutil.ReadAll(questionsFile)
+	if err := PiSquared.LoadQuestions(questionsValue); err != nil {
+		log.Fatal("Error parsing questions file")
 	}
 
 	bot, err := PiSquared.NewBot(os.Getenv("BOT_TOKEN"))
